d0eb7a73337fb0f9c11668d2ebc18e662490b8b8
amankrgupta97 <70031251+amankrgupta97@users.noreply.github.com>
2023-01-11 09:53:17 +0000

    completed password complexity function

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,7 +5,23 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        if(password.length()<6){
+            return false;
+        }
+        boolean hasUpperCase=false;
+        boolean hasLowerCase=false;
+        boolean hasDigit=false;
+        for(int i=0;i<password.length() || !hasUpperCase && !hasLowerCase && !hasDigit;i++){
+            Character current=password.charAt(i);
+            if(Character.isDigit(current)){
+                hasDigit=true;
+            }else if(Character.isUpperCase(current)){
+                hasUpperCase=true;
+            }else if(Character.isLowerCase(current)){
+                hasLowerCase=true;
+            }
+        }
+        return hasDigit && hasUpperCase && hasLowerCase;
     }
 
     public static void main(String[] args) {
