7bf081a305535f582b7575db8ff01098734eceb0
amankrgupta97 <70031251+amankrgupta97@users.noreply.github.com>
2023-01-11 07:43:35 +0000

    isEven function completed

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class App {
     public static boolean isEven(int n) {
-        // todo: implement functionality
+        if(n%2==0){
+            return true;
+        }
         return false;
     }
 
