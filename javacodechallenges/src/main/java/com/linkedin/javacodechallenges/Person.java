ff6db63eebac4283cdf7fe4ec09114546de3156f
amankrgupta97 <70031251+amankrgupta97@users.noreply.github.com>
2023-01-12 09:12:36 +0000

    completed person class

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,44 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
-  
+
+  private String firstName;
+  private String lastName;
+  private int age;
+
+  public Person(String firstName, String lastName, int age){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.age=age;
+  }
+
+  public String getFirstName(){
+    return this.firstName;
+  }
+
+  public void setFirstName(String firstName){
+    this.firstName=firstName;
+  }
+
+  public String getLastName(){
+    return this.lastName;
+  }
+
+  public void setLastName(String lastName){
+    this.lastName=lastName;
+  }
+
+  public int getAge(){
+    return this.age;
+  }
+
+  public void setAge(int age){
+    if(age<this.age){
+      System.out.println("You Can't make Person Younger");
+    }
+    this.age=age;
+  }
+  public void introduction(){
+    System.out.println("Hello my name is "+this.firstName+" "+this.lastName+" I'm "+this.age+" Years Old");
+  }
 }
