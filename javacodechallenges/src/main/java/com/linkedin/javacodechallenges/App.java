0c14426b266e8033b54ece0e1d136fe4f63097aa
amankrgupta97 <70031251+amankrgupta97@users.noreply.github.com>
2023-01-12 12:28:07 +0000

    implemented findStudent function

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,5 +1,6 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -7,8 +8,27 @@ public class App {
     public static List<String> findStudentsWithIncompleteVolunteerEvents(
             List<String> students,
             Map<String, List<String>> attendeesMapping) {
-        // TODO: implement function
-        return List.of();
+                List<String> studentsWithIncompleteVolunteer = new ArrayList<String>();;
+                int count=0;
+                for(String student :students){
+                        for(List<String> eventAttendees:attendeesMapping.values()){
+                                for(String attendee:eventAttendees){
+                                        if(student.equals(attendee)){
+                                                count=count+1;
+                                                break;
+                                        }
+                                }
+                                if(count>=2){
+                                        break;
+                                }
+                        }
+                        if(count<2){
+                                studentsWithIncompleteVolunteer.add(student);
+                        }
+                        count=0;
+                }
+        
+        return studentsWithIncompleteVolunteer;
     }
 
     public static void main(String[] args) {
