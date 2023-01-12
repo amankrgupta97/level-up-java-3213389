92d41628985de49453010410f49d685cabd53519
amankrgupta97 <70031251+amankrgupta97@users.noreply.github.com>
2023-01-12 08:44:36 +0000

    completed calculate water bill

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,12 +1,29 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class App {
 
     public static double calculateWaterBill(double gallonsUsage) {
-        // TODO: Implement method
-        return 0;
+        int gallonsPerCcf=748;
+        final double COST_PER_2CCF =18.84;
+        final double COST_PER_ADD_CCF=3.90;
+        double bill=0.0;
+        if(gallonsUsage>(2*gallonsPerCcf)){
+
+            double additonalGallonUsage=(gallonsUsage-2*gallonsPerCcf)/gallonsPerCcf;
+
+            if(Math.round(additonalGallonUsage)-additonalGallonUsage < 0){
+                additonalGallonUsage=Math.round(additonalGallonUsage)+1;
+            }
+            else{
+                additonalGallonUsage=Math.round(additonalGallonUsage);
+            }
+            bill=COST_PER_2CCF+additonalGallonUsage*COST_PER_ADD_CCF;
+            return bill;
+        }
+        return COST_PER_2CCF;
     }
 
     public static void main(String[] args) {
