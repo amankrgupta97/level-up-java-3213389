cdcfc877a397e31f98e2e5967d936e4c8fcf8f64
amankrgupta97 <70031251+amankrgupta97@users.noreply.github.com>
2023-01-19 05:07:00 +0000

    implemented redact Text File

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,12 +1,41 @@
 package com.linkedin.javacodechallenges;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class App 
 {
-    public static void redactTextFile(String fileName,
-                                      String[] redactedWordsArray) {
+    public static void redactTextFile(String fileName,String[] redactedWordsArray) {
+        if(!fileName.contains(".txt")){
+            System.out.println("This is not a text file");
+            return;
+        }
+        try{
+            File originalFile = new File(fileName);
+            BufferedReader originalFileReader = new BufferedReader(new FileReader(originalFile));
+            File redactedFile = new File("redacted-"+fileName);
+            BufferedWriter redactedBufferedWriter = new BufferedWriter(new FileWriter(redactedFile));
+            String currentLine = originalFileReader.readLine();
 
+            while(currentLine!=null){
+                for(String redactedWord: redactedWordsArray){
+                    currentLine= StringUtils.replaceIgnoreCase(currentLine,redactedWord,"REDACTED");
+                }
+                redactedBufferedWriter.append(currentLine).append("\n");
+                currentLine=originalFileReader.readLine();
+            }
+            originalFileReader.close();
+            redactedBufferedWriter.close();
+        }catch(IOException e){
+            System.out.println("Problem in reading and writing file"+e);
+        }
     }
 
     public static void main(String[] args) {
